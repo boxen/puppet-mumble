18bb9e390c906969b0853537427435d425ac64ac
petems <p.morsou@gmail.com>
2013-04-16 14:27:42 +0100

    Initial commit for Mumble

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install Mumble into /Applications
+#
+# Usage:
+#
+#     include mumble
+class mumble {
+  package { 'Mumble':
+    provider => 'appdmg',
+    source   => 'http://kent.dl.sourceforge.net/project/mumble/Mumble/1.2.3a/Mumble-1.2.3a.dmg';
+  }
 }
